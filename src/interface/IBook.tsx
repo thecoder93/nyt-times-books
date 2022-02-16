9bd7dca9c35047594e136782dfd977adbdcd5a25
Hanslukas <gl.lamanna@gmail.com>
2022-02-16 22:58:58 +0100

    add interface book object

---
+++ src/interface/IBook.tsx
@@ -0,0 +1,13 @@
+export interface IBook {
+    author?: string;
+    contributor?: string;
+    description?: string;
+    book_image?: string;
+    buy_links?: string[];
+    primary_isbn13?: string;
+    primary_isbn10?: string;
+    publisher?: string;
+    rank?: number;
+    title?: string;
+    weeks_on_list?: string;
+}
