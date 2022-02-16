64ea9b879c8f38ed0ee324365270b939366ae7df
Hanslukas <gl.lamanna@gmail.com>
2022-02-16 22:58:27 +0100

    fix url api getBook

--- src/services/OverviewBooksServices.tsx
+++ src/services/OverviewBooksServices.tsx
@@ -3,7 +3,7 @@ import axios from "axios";
 const NYT_APP_API_KEY = process.env.REACT_APP_NYT_APP_API_KEY  
 const BASE_URL = process.env.REACT_APP_BASE_URL_API_NYT  
 
-export const API_URL = `${BASE_URL}/lists/overview.json?apiKey=${NYT_APP_API_KEY}`
+export const API_URL = `${BASE_URL}/lists/overview.json?api-key=${NYT_APP_API_KEY}`
 
 export const getOverviewBooks = async () => {
   try {
@@ -12,4 +12,3 @@ export const getOverviewBooks = async () => {
     return [];
   }
 };
-
