e052f5d492456e2eea85c1d600e69298c3f55d8a
Hanslukas <gl.lamanna@gmail.com>
2022-02-17 16:16:05 +0100

    clean code and remove comment

--- functions/books.js
+++ functions/books.js
@@ -2,7 +2,7 @@ const { default: axios } = require("axios")
 
 exports.handler = async function(event, callback) {
 
-    const NYT_APP_API_KEY = process.env.REACT_APP_NYT_APP_API_KEY  
+  const NYT_APP_API_KEY = process.env.REACT_APP_NYT_APP_API_KEY  
   const BASE_URL = process.env.REACT_APP_BASE_URL_API_NYT  
       
   const API_URL = `${BASE_URL}/lists/overview.json?api-key=${NYT_APP_API_KEY}`
@@ -19,26 +19,4 @@ exports.handler = async function(event, callback) {
           body: JSON.stringify(error)
       }
     }
-
-//     const pass = (body) => {callback(null, {statusCode: 200, body: JSON.stringify(body)})}
-
-//     const NYT_APP_API_KEY = process.env.REACT_APP_NYT_APP_API_KEY  
-//     const BASE_URL = process.env.REACT_APP_BASE_URL_API_NYT  
-    
-//     const API_URL = `${BASE_URL}/lists/overview.json?api-key=${NYT_APP_API_KEY}`
-//   try {
-//     let response = await fetch(API_URL, 
-//   {
-//    method: event.httpMethod,
-//    body: event.body
-//   })
-//    let data = await response.json()
-//    await pass(data)
-//  } catch(err) {
-//      let error = {
-//        statusCode: err.statusCode || 500,
-//        body: JSON.stringify({error: err.message})
-//  }
-//   await pass(error)
-//  }
 }
