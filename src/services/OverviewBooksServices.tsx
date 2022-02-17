193c299e34d232a4652b77f5fd307c26378dca91
Hanslukas <gl.lamanna@gmail.com>
2022-02-17 17:03:50 +0100

    endpoint for netlify

--- src/services/OverviewBooksServices.tsx
+++ src/services/OverviewBooksServices.tsx
@@ -2,8 +2,8 @@ import axios from "axios";
  const NYT_APP_API_KEY = process.env.REACT_APP_NYT_APP_API_KEY  
  const BASE_URL = process.env.REACT_APP_BASE_URL_API_NYT  
 
-export const API_URL = `${BASE_URL}/lists/overview.json?api-key=${NYT_APP_API_KEY}`
-//export const API_URL2 = `/.netlify/functions/books`
+//export const API_URL = `${BASE_URL}/lists/overview.json?api-key=${NYT_APP_API_KEY}`
+export const API_URL = `/.netlify/functions/books`
 
 export const getOverviewBooks = async () => {
   try {
