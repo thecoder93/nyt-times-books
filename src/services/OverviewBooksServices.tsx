a3a227297a1218196746998a8fce5f83fc58dd32
Hanslukas <gl.lamanna@gmail.com>
2022-02-17 17:08:36 +0100

    remove unsed variables

--- src/services/OverviewBooksServices.tsx
+++ src/services/OverviewBooksServices.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
- const NYT_APP_API_KEY = process.env.REACT_APP_NYT_APP_API_KEY  
- const BASE_URL = process.env.REACT_APP_BASE_URL_API_NYT  
+//  const NYT_APP_API_KEY = process.env.REACT_APP_NYT_APP_API_KEY  
+//  const BASE_URL = process.env.REACT_APP_BASE_URL_API_NYT  
 
 //export const API_URL = `${BASE_URL}/lists/overview.json?api-key=${NYT_APP_API_KEY}`
 export const API_URL = `/.netlify/functions/books`
