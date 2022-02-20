ed74580d2fecfe0cc077b215f58d01c24d8741ab
Hanslukas <gl.lamanna@gmail.com>
2022-02-20 13:36:54 +0100

    refactor searchInput

--- src/components/Search/SearchInput.tsx
+++ src/components/Search/SearchInput.tsx
@@ -1,18 +1,15 @@
-import { useContext, useState } from 'react';
+import { useContext } from 'react';
 import { BooksContext } from '../ListBooks/ListBooks';
 import './SearchInput.css'
 
 
 const SearchInput = () => {
     
-    const  { category, setCategory} = useContext(BooksContext)
-
-    const [word, setWord] = useState("");
+    const  { search , setSearch} = useContext(BooksContext)
 
     const handleChange = (e: { target: { value: any } }) => {
         const { value } = e.target;
-        setWord(value);
-        setCategory(getMatchedList(word, category))
+        setSearch(value);
       };
     
   return (
@@ -22,27 +19,10 @@ const SearchInput = () => {
         className="search-input"
         placeholder="Cerca libro..."
         onChange={handleChange}
-        value={word}
+        value={search}
       />
     </>
   );
 }
-
-
-const getMatchedList = (searchText: string, listOfBooks: any[]) => {
-    console.log("LIBRI", listOfBooks)
-    if (searchText === "") return listOfBooks;
-    return listOfBooks.map((element) => {
-        return {...element, books: element.books.filter((books: any) => books.title.toLowerCase().includes(searchText.toLowerCase()))}
-      })
-
-  };
-
-
-// const getMatchedList = (searchText: string, listOfBooks: any[]) => {
-//     listOfBooks = listOfBooks.filter((item) => {
-//         return Object.keys(item).some(key => item[key].toString().search(searchText) !== -1);
-//     });
-// }
 
 export default SearchInput
